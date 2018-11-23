01b8999ceafddcc1f0ba4775bc1ac318e9e51dc7
lorenzo <lolo.tomaz@gmail.com>
2018-11-23 15:57:43 -0300

    setimo commit contendo alteracoes no arquivo entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,9 +2,9 @@ import media
 import fresh_tomatoes
 
 
-narradores_de_jave = media.Movie("narradores de jave", "Ao saber que Javé pode desaparecer sob as águas de uma hidrelétrica, os moradores do vilarejo decidem escrever sua história e transformar o local em patrimônio a ser preservado.", "https://upload.wikimedia.org/wikipedia/pt/3/35/Narradores_de_Jav%C3%A9.jpg", "https://www.youtube.com/watch?v=GlaFRraqeOg")
+narradores_de_jave = media.Movie("Narradores de Javé", "Ao saber que Javé pode desaparecer sob as águas de uma hidrelétrica, os moradores do vilarejo decidem escrever sua história e transformar o local em patrimônio a ser preservado.", "https://upload.wikimedia.org/wikipedia/pt/3/35/Narradores_de_Jav%C3%A9.jpg", "https://www.youtube.com/watch?v=GlaFRraqeOg")
 
-era_o_hotel_cambridge = media.Movie("era o hotel cambridge", "Refugiados recém-chegados ao Brasil dividem com um grupo de sem-tetos um velho edifício abandonado no centro de São Paulo. Os novos moradores do prédio têm que lidar com seus dramas pessoais e aprender a conviver com pessoas que, apesar de diferentes, enfrentam juntos a vida nas ruas.", "http://www.atoupeira.com.br/wp-content/uploads/2016/10/Era-o-Hotel-Cambridge-Festival-do-Rio.png", "https://www.youtube.com/watch?v=a5EQUG-RMI8")
+era_o_hotel_cambridge = media.Movie("Era o hotel cambridge", "Refugiados recém-chegados ao Brasil dividem com um grupo de sem-tetos um velho edifício abandonado no centro de São Paulo. Os novos moradores do prédio têm que lidar com seus dramas pessoais e aprender a conviver com pessoas que, apesar de diferentes, enfrentam juntos a vida nas ruas.", "http://www.atoupeira.com.br/wp-content/uploads/2016/10/Era-o-Hotel-Cambridge-Festival-do-Rio.png", "https://www.youtube.com/watch?v=a5EQUG-RMI8")
 
 o_auto_da_compadecida = media.Movie("O auto da compadecida", "O filme mostra as aventuras de João Grilo e Chicó, dois nordestinos pobres que vivem de golpes para sobreviver. Eles estão sempre enganando o povo de um pequeno vilarejo no sertão da Paraíba, inclusive o temido cangaceiro Severino de Aracaju, que os persegue pela região. Somente a aparição da Nossa Senhora poderá salvar esta dupla.", "https://upload.wikimedia.org/wikipedia/pt/b/bf/O_auto_da_compadecida.jpg", "https://www.youtube.com/watch?v=XPuMu_ENzlg")
 
@@ -17,7 +17,7 @@ kenoma = media.Movie("Kenoma", "Um artesão de um pequeno povoado tenta construi
 
 
 #cria uma lista com instâncias da classe Movie do arquivo media.py
-movies = [narradores_de_jave, era_o_hotel_cambridge, o_auto_da_compadecida, o_homem_que_virou_suco, central_do_brasil]
+movies = [narradores_de_jave, era_o_hotel_cambridge, o_auto_da_compadecida, o_homem_que_virou_suco, central_do_brasil, kenoma]
 
 # inicia a função open_movies_page do arquivo fresh_tomatoes.py
 fresh_tomatoes.open_movies_page(movies)
